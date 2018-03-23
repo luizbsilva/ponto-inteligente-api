3648137f7a42321c5a75394afc83135f4df998f7
Jose Luiz <luizbsilva@gmail.com>
2018-03-23 10:08:25 -0300

    Correção da Classe PontoInteligenteApplication

--- src/main/java/com/kairos/pontointeligente/api/PontoInteligenteApplication.java
+++ src/main/java/com/kairos/pontointeligente/api/PontoInteligenteApplication.java
@@ -1,17 +1,14 @@
 package com.kairos.pontointeligente.api;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@ActiveProfiles("test")
+@SpringBootApplication
+@EnableCaching
 public class PontoInteligenteApplication {
 
-	@Test
-	public void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(PontoInteligenteApplication.class, args);
 	}
 }
